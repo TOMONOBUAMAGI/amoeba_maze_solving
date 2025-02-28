7d2fe8bb4182ef3411c9fcd10e5b9a8f7b1fd2ba
amagitomonobu <tomonobu.amagi@rooter.co.jp>
2025-02-28 16:30:36 +0900

    迷路をループ有にする

--- functions.js
+++ functions.js
@@ -9,24 +9,22 @@ function ConstructMaze() {
       } else if(x%2==0 && y%2==0 && x<mazeWidth-1 && y<mazeHeight) { // 内壁
         let neighborCellX;
         let neighborCellY;
-        while (true) {
-          const randomN = Math.floor(Math.random() * directionNum);
-          if (randomN==0) { // 右
-            neighborCellX = x+1;
-            neighborCellY = y;
-          } else if(randomN==1) { // 下
-            neighborCellX = x;
-            neighborCellY = y+1;
-          } else if(randomN==2) { // 左
-            neighborCellX = x-1;
-            neighborCellY = y;
-          } else { // 上
-            neighborCellX = x;
-            neighborCellY = y-1;
-          }
 
-          if(!returnArray[neighborCellY][neighborCellX]) break;
+        const randomN = Math.floor(Math.random() * directionNum);
+        if (randomN==0) { // 右
+          neighborCellX = x+1;
+          neighborCellY = y;
+        } else if(randomN==1) { // 下
+          neighborCellX = x;
+          neighborCellY = y+1;
+        } else if(randomN==2) { // 左
+          neighborCellX = x-1;
+          neighborCellY = y;
+        } else { // 上
+          neighborCellX = x;
+          neighborCellY = y-1;
         }
+
         returnArray[y][x] = true;
         returnArray[neighborCellY][neighborCellX] = true;
       }
